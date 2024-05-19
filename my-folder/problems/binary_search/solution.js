48a5a8a23b8bc529ef7754664c05f04fe1613da9
Sora <88293207+sora19961102@users.noreply.github.com>
2024-05-19 15:47:30 +0000

    Sync LeetCode submission - Binary Search (javascript)

---
+++ my-folder/problems/binary_search/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0,
+        end = nums.length - 1
+    
+    while (start <= end) {
+        let mid = start + Math.floor((end - start) / 2)
+
+        if(nums[mid] == target) {
+            return mid
+        } else if (nums[mid] < target) {
+            start = mid + 1
+        } else {
+            end = mid - 1
+        }
+    }
+
+    return -1
+};
